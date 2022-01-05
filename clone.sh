d934ee92bd6658c35fd7803cb3e193c07f59c481
Hasin Abrar Haque <70853665+AbrarNoob@users.noreply.github.com>
2022-01-05 19:29:24 +0600

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -7,7 +7,7 @@
      #rm -rf hardware/qcom-caf/msm8996/media
    
    #remove_old_trees
-      rm -rf device/xiaomi/lavender
+     #rm -rf device/xiaomi/lavender
      #rm -rf vendor/xiaomi/lavender
      #rm -rf kernel/xiaomi/lavender
    
@@ -16,7 +16,7 @@
      
    #clone files
      #git clone https://github.com/AbrarNoob/platform_vendor_qcom_opensource_vibrator -b 12 vendor/qcom/opensource/vibrator
-     #git clone https://github.com/AbrarNoob/device_qcom_sepolicy-legacy-um device/qcom/sepolicy-legacy-um
+      git clone https://github.com/AbrarNoob/device_qcom_sepolicy-legacy-um -b 12 device/qcom/sepolicy-legacy-um
 
    #clone_clang
      #git clone https://github.com/kdrag0n/proton-clang -b master prebuilts/clang/host/linux-x86/clang-proton --depth=1
@@ -27,6 +27,6 @@
      #git clone https://github.com/AbrarNoob/android_hardware_qcom_display -b 12 hardware/qcom-caf/msm8998/display
     
     #clone_trees
-      git clone https://github.com/AbrarNoob/device_xiaomi_lavender-S -b syb device/xiaomi/lavender
+     #git clone https://github.com/AbrarNoob/device_xiaomi_lavender-S -b syb device/xiaomi/lavender
      #git clone https://github.com/AbrarNoob/android_vendor_xiaomi_lavender-S -b evo vendor/xiaomi/lavender
      #git clone https://github.com/Projects-aRise/android_kernel_xiaomi_lavender-LTO -b eas-main kernel/xiaomi/lavender
