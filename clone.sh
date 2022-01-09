ca3d337f99cdc168e4924e4c9edeecf2e368c02a
Hasin Abrar Haque <70853665+AbrarNoob@users.noreply.github.com>
2022-01-09 14:43:40 +0600

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -28,6 +28,6 @@
      #git clone https://github.com/AbrarNoob/android_hardware_qcom_display -b 12 hardware/qcom-caf/msm8998/display
     
     #clone_trees
-      git clone https://github.com/AbrarNoob/device_xiaomi_lavender-S -b arc device/xiaomi/lavender
+      git clone https://github.com/AbrarNoob/device_xiaomi_lavender-S -b arct device/xiaomi/lavender
       #git clone https://github.com/AbrarNoob/android_vendor_xiaomi_lavender-S -b evo vendor/xiaomi/lavender
       #git clone https://github.com/stormbreaker-project/kernel_xiaomi_lavender -b oldcam-eas kernel/xiaomi/lavender
