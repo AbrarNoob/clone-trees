9bbc2419eeeeb885dd9b2461fa0c8af4b0285143
Hasin Abrar Haque <70853665+AbrarNoob@users.noreply.github.com>
2022-01-15 02:33:55 +0600

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -6,24 +6,24 @@
      #rm -rf hardware/qcom-caf/msm8998/media
    
    #remove_old_trees
-     #rm -rf device/xiaomi/lavender
-     #rm -rf vendor/xiaomi/lavender
-     #rm -rf kernel/xiaomi/lavender
+     rm -rf device/xiaomi/lavender
+     rm -rf vendor/xiaomi/lavender
+     rm -rf kernel/xiaomi/lavender
 
    #replace source files
-     #rm -rf device/qcom/sepolicy-legacy-um
-     #rm -rf prebuilts/clang/host/linux-x86/clang-proton
-     #rm -rf system/sepolicy
+      rm -rf vendor/banana
+      rm -rf prebuilts/clang/host/linux-x86/clang-proton
+      rm -rf packages/apps/Dialer
+      rm -rf frameworks/base
 
    #clone files
 
-     #git clone https://github.com/AbrarNoob/platform_vendor_qcom_opensource_vibrator -b 12 vendor/qcom/opensource/vibrator
-     #git clone https://github.com/VoltageOS/device_qcom_sepolicy-legacy-um -b 12 device/qcom/sepolicy-legacy-um
-     #git clone https://github.com/Spark-Rom/system_sepolicy -b spark system/sepolicy
-      git clone https://github.com/ArrowOS/android_external_ant-wireless_antradio-library -b arrow-12.0 external/ant-wireless/antradio-library
- 
+      git clone https://github.com/AbrarNoob/android_frameworks_base -b 11 frameworks/base
+      git clone https://github.com/AbrarNoob/android_vendor_banana -b 11 vendor/banana
+      git clone https://github.com/Octavi-OS/platform_packages_apps_Dialer -b 11 packages/apps/Dialer
+
    #clone_clang
-     #git clone https://github.com/kdrag0n/proton-clang -b master prebuilts/clang/host/linux-x86/clang-proton --depth=1
+      git clone https://github.com/kdrag0n/proton-clang -b master prebuilts/clang/host/linux-x86/clang-proton --depth=1
 
    #clone_HALS
      #git clone https://github.com/AbrarNoob/android_hardware_qcom_media -b 12 hardware/qcom-caf/msm8998/media
@@ -31,6 +31,6 @@
      #git clone https://github.com/AbrarNoob/android_hardware_qcom_display -b 12 hardware/qcom-caf/msm8998/display
     
     #clone_trees
-      #git clone https://github.com/AbrarNoob/device_xiaomi_lavender-S -b xtended device/xiaomi/lavender
-      #git clone https://github.com/AbrarNoob/android_vendor_xiaomi_lavender-S -b twelve vendor/xiaomi/lavender
-      #git clone https://github.com/stormbreaker-project/kernel_xiaomi_lavender -b oldcam-eas kernel/xiaomi/lavender
+      git clone https://github.com/AbrarNoob/android_device_xiaomi_lavender -b banana device/xiaomi/lavender
+      git clone https://github.com/AbrarNoob/android_vendor_xiaomi_lavender -b eleven vendor/xiaomi/lavender
+      git clone https://github.com/stormbreaker-project/kernel_xiaomi_lavender -b oldcam-eas kernel/xiaomi/lavender
