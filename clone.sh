518f8e678c42a0fea621fb2baae3bafb4b3598aa
Hasin Abrar Haque <70853665+AbrarNoob@users.noreply.github.com>
2022-01-15 03:10:59 +0600

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -7,23 +7,23 @@
    
    #remove_old_trees
      rm -rf device/xiaomi/lavender
-     rm -rf vendor/xiaomi/lavender
-     rm -rf kernel/xiaomi/lavender
+     #rm -rf vendor/xiaomi/lavender
+     #rm -rf kernel/xiaomi/lavender
 
    #replace source files
-      rm -rf vendor/banana
-      rm -rf prebuilts/clang/host/linux-x86/clang-proton
-      rm -rf packages/apps/Dialer
-      rm -rf frameworks/base
+      #rm -rf vendor/banana
+      #rm -rf prebuilts/clang/host/linux-x86/clang-proton
+      #rm -rf packages/apps/Dialer
+      #rm -rf frameworks/base
 
    #clone files
 
-      git clone https://github.com/AbrarNoob/android_frameworks_base -b 11 frameworks/base
-      git clone https://github.com/AbrarNoob/android_vendor_banana -b 11 vendor/banana
-      git clone https://github.com/Octavi-OS/platform_packages_apps_Dialer -b 11 packages/apps/Dialer
+      #git clone https://github.com/AbrarNoob/android_frameworks_base -b 11 frameworks/base
+      #git clone https://github.com/AbrarNoob/android_vendor_banana -b 11 vendor/banana
+      #git clone https://github.com/Octavi-OS/platform_packages_apps_Dialer -b 11 packages/apps/Dialer
 
    #clone_clang
-      git clone https://github.com/kdrag0n/proton-clang -b master prebuilts/clang/host/linux-x86/clang-proton --depth=1
+      #git clone https://github.com/kdrag0n/proton-clang -b master prebuilts/clang/host/linux-x86/clang-proton --depth=1
 
    #clone_HALS
      #git clone https://github.com/AbrarNoob/android_hardware_qcom_media -b 12 hardware/qcom-caf/msm8998/media
@@ -32,5 +32,5 @@
     
     #clone_trees
       git clone https://github.com/AbrarNoob/android_device_xiaomi_lavender -b banana device/xiaomi/lavender
-      git clone https://github.com/AbrarNoob/android_vendor_xiaomi_lavender -b eleven vendor/xiaomi/lavender
-      git clone https://github.com/stormbreaker-project/kernel_xiaomi_lavender -b oldcam-eas kernel/xiaomi/lavender
+      #git clone https://github.com/AbrarNoob/android_vendor_xiaomi_lavender -b eleven vendor/xiaomi/lavender
+      #git clone https://github.com/stormbreaker-project/kernel_xiaomi_lavender -b oldcam-eas kernel/xiaomi/lavender
