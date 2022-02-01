d3eed257382a5d76715900bed40e01bf56853e44
Hasin Abrar Haque <70853665+AbrarNoob@users.noreply.github.com>
2022-02-01 23:52:34 +0600

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
   #remove_HALS
-     #rm -rf hardware/qcom-caf/msm8998/audio
-     #rm -rf hardware/qcom-caf/msm8998/display
-     #rm -rf hardware/qcom-caf/msm8998/media
+     rm -rf hardware/qcom-caf/msm8998/audio
+     rm -rf hardware/qcom-caf/msm8998/display
+     rm -rf hardware/qcom-caf/msm8998/media
    
    #remove_old_trees
       rm -rf device/xiaomi/lavender
-      #rm -rf vendor/xiaomi/lavender
-      #rm -rf kernel/xiaomi/lavender
+      rm -rf vendor/xiaomi/lavender
+      rm -rf kernel/xiaomi/lavender
 
    #replace source files
       #rm -rf vendor/banana
-      #rm -rf prebuilts/clang/host/linux-x86/clang-proton
+      rm -rf prebuilts/clang/host/linux-x86/clang-proton
       #rm -rf packages/apps/Dialer
       #rm -rf frameworks/base
        #rm -rf external/ant-wireless/antradio-library  
@@ -26,14 +26,14 @@
       #git clone https://github.com/ArrowOS/android_external_ant-wireless_antradio-library -b arrow-12.0 external/ant-wireless/antradio-library
 
    #clone_clang
-     #git clone https://github.com/kdrag0n/proton-clang -b master prebuilts/clang/host/linux-x86/clang-proton --depth=1
+     git clone https://github.com/kdrag0n/proton-clang -b master prebuilts/clang/host/linux-x86/clang-proton --depth=1
 
    #clone_HALS
-     #git clone https://github.com/AbrarNoob/android_hardware_qcom_media -b 12 hardware/qcom-caf/msm8998/media
-     #git clone https://github.com/AbrarNoob/android_hardware_qcom_audio -b 12 hardware/qcom-caf/msm8998/audio
-     #git clone https://github.com/AbrarNoob/android_hardware_qcom_display -b 12 hardware/qcom-caf/msm8998/display
+     git clone https://github.com/AbrarNoob/android_hardware_qcom_media -b 12 hardware/qcom-caf/msm8998/media
+     git clone https://github.com/AbrarNoob/android_hardware_qcom_audio -b 12 hardware/qcom-caf/msm8998/audio
+     git clone https://github.com/AbrarNoob/android_hardware_qcom_display -b 12 hardware/qcom-caf/msm8998/display
     
     #clone_trees
-      git clone https://github.com/AbrarNoob/device_xiaomi_lavender-S -b anc device/xiaomi/lavender
-      #git clone https://github.com/AbrarNoob/android_vendor_xiaomi_lavender-S -b twelve vendor/xiaomi/lavender
-      #git clone https://github.com/AbrarNoob/kernel_xiaomi_lavender -b qti-oldcam-eas kernel/xiaomi/lavender
+      git clone https://github.com/AbrarNoob/device_xiaomi_lavender-S -b ancient device/xiaomi/lavender
+      git clone https://github.com/AbrarNoob/android_vendor_xiaomi_lavender-S -b twelve vendor/xiaomi/lavender
+      git clone https://github.com/MukulSharma06175/kernel-xiaomi-lavender -b main kernel/xiaomi/lavender
