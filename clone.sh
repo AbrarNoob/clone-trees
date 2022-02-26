a46d06f75aa00e56a5df88a186dd664844a771c0
Hasin Abrar Haque <70853665+AbrarNoob@users.noreply.github.com>
2022-02-27 01:02:43 +0600

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
   #remove_HALS
-     #rm -rf hardware/qcom-caf/msm8998/audio
-     #rm -rf hardware/qcom-caf/msm8998/display
-     #rm -rf hardware/qcom-caf/msm8998/media
+     rm -rf hardware/qcom-caf/msm8998/audio
+     rm -rf hardware/qcom-caf/msm8998/display
+     rm -rf hardware/qcom-caf/msm8998/media
    
    #remove_old_trees
       rm -rf device/xiaomi/lavender
@@ -29,9 +29,9 @@
      #git clone https://github.com/kdrag0n/proton-clang -b master prebuilts/clang/host/linux-x86/clang-proton --depth=1
 
    #clone_HALS
-     #git clone https://github.com/AbrarNoob/android_hardware_qcom_media -b 12 hardware/qcom-caf/msm8998/media
-     #git clone https://github.com/AbrarNoob/android_hardware_qcom_audio -b 12 hardware/qcom-caf/msm8998/audio
-     #git clone https://github.com/AbrarNoob/android_hardware_qcom_display -b 12 hardware/qcom-caf/msm8998/display
+     git clone https://github.com/AbrarNoob/android_hardware_qcom_media -b 12 hardware/qcom-caf/msm8998/media
+     git clone https://github.com/AbrarNoob/android_hardware_qcom_audio -b 12 hardware/qcom-caf/msm8998/audio
+     git clone https://github.com/AbrarNoob/android_hardware_qcom_display -b 12 hardware/qcom-caf/msm8998/display
     
    
    #clone_trees
