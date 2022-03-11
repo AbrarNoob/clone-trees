074d4928ab3cd4c63304dd0336409af0c637ce9d
Hasin Abrar Haque <70853665+AbrarNoob@users.noreply.github.com>
2022-03-11 19:27:27 +0600

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -7,15 +7,15 @@
    
    #remove_old_trees
       rm -rf device/xiaomi/lavender
-      #rm -rf vendor/xiaomi/lavender
-      #rm -rf kernel/xiaomi/lavender
+      rm -rf vendor/xiaomi/lavender
+      rm -rf kernel/xiaomi/lavender
 
    #replace source files
         #rm -rf build/soong
         #rm -rf frameworks/base
       #rm -rf vendor/octavi
        #rm -rf system/sepolicy
-       #rm -rf prebuilts/clang/host/linux-x86/clang-proton
+       rm -rf prebuilts/clang/host/linux-x86/clang-proton
       #rm -rf packages/apps/Dialer
       #rm -rf frameworks/base
       rm -rf external/ant-wireless/antradio-library  
@@ -25,10 +25,10 @@
        #git clone https://github.com/Octavi-OS/platform_frameworks_base -b 12 frameworks/base
       #git clone https://github.com/AbrarNoob/platform_system_sepolicy -b test system/sepolicy
       #git clone https://github.com/AbrarNoob/vendor_octavi -b 12.1 vendor/octavi
-      git clone https://github.com/ArrowOS/android_external_ant-wireless_antradio-library -b arrow-12.0 external/ant-wireless/antradio-library
+      git clone https://github.com/LineageOS/android_external_ant-wireless_antradio-library -b lineage-19.0 external/ant-wireless/antradio-library
 
    #clone_clang
-     #git clone https://github.com/kdrag0n/proton-clang -b master prebuilts/clang/host/linux-x86/clang-proton --depth=1
+     git clone https://github.com/kdrag0n/proton-clang -b master prebuilts/clang/host/linux-x86/clang-proton --depth=1
 
    #clone_HALS
      git clone https://github.com/AbrarNoob/android_hardware_qcom_media -b 12 hardware/qcom-caf/msm8998/media
@@ -38,8 +38,8 @@
    
    #clone_trees
       git clone https://github.com/AbrarNoob/device_xiaomi_lavender-S -b evo device/xiaomi/lavender
-       #git clone https://github.com/AbrarNoob/android_vendor_xiaomi_lavender-S -b evo vendor/xiaomi/lavender
-      #git clone https://github.com/ancient-devices/kernel_xiaomi_lavender -b twelve kernel/xiaomi/lavender
+      git clone https://github.com/AbrarNoob/android_vendor_xiaomi_lavender-S -b evo vendor/xiaomi/lavender
+      git clone https://github.com/ancient-devices/kernel_xiaomi_lavender -b twelve kernel/xiaomi/lavender
 
    #misc
       #export SELINUX_IGNORE_NEVERALLOWS=true
