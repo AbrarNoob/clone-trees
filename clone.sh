4aefca5d388b921de0118c914d2c12b6a95ebeb2
Hasin Abrar Haque <70853665+AbrarNoob@users.noreply.github.com>
2022-03-21 10:41:00 +0600

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -8,7 +8,7 @@
    #remove_old_trees
       rm -rf device/xiaomi/lavender
       rm -rf vendor/xiaomi/lavender
-      #rm -rf kernel/xiaomi/lavender
+      rm -rf kernel/xiaomi/lavender
 
    #replace source files
         #rm -rf frameworks/base
@@ -26,7 +26,7 @@
       #git clone https://github.com/ArrowOS/android_external_ant-wireless_antradio-library -b arrow-12.0 external/ant-wireless/antradio-library
 
    #clone_clang
-     #git clone https://github.com/kdrag0n/proton-clang -b master prebuilts/clang/host/linux-x86/clang-proton --depth=1
+     git clone https://github.com/kdrag0n/proton-clang -b master prebuilts/clang/host/linux-x86/clang-proton --depth=1
 
    #clone_HALS
      git clone https://github.com/AbrarNoob/android_hardware_qcom_media -b 12 hardware/qcom-caf/msm8998/media
@@ -35,9 +35,9 @@
     
    
    #clone_trees
-      git clone https://github.com/AbrarNoob/device_xiaomi_lavender-S -b ancient device/xiaomi/lavender
+      git clone https://github.com/AbrarNoob/device_xiaomi_lavender-S -b pal device/xiaomi/lavender
       git clone https://github.com/AbrarNoob/android_vendor_xiaomi_lavender-S -b twelve vendor/xiaomi/lavender
-      #git clone https://github.com/ancient-devices/kernel_xiaomi_lavender -b twelve kernel/xiaomi/lavender
+      git clone https://github.com/ancient-devices/kernel_xiaomi_lavender -b twelve kernel/xiaomi/lavender
 
    #misc
-      export FORCE_LAWNCHAIR=true
+      #export FORCE_LAWNCHAIR=true
